a7a16dee755300feca72edef82d99ff5634b8aea
xergale <xergale@gmail.com>
2026-08-27 09:01:07 -0700

    started art hookup. added sprite swapper for totem.

---
+++ Assets/scripts/SpriteStateSwapper.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class SpriteStateSwapper : MonoBehaviour
+{
+    [SerializeField] private Sprite[] sprites;
+    [SerializeField] private int _spriteIndex;
+    public int spriteIndex
+    {
+        get { return _spriteIndex; }
+        set 
+        { 
+            _spriteIndex = Mathf.Clamp(value, 0, sprites.Length-1); 
+            GetComponent<SpriteRenderer>().sprite = sprites[_spriteIndex];
+        }
+    }
+    private void OnValidate()
+    {
+        spriteIndex = _spriteIndex;
+    }
+}
