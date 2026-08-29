c3fd555097abde875a6fc4000b3d4506fad19b2f
xergale <xergale@gmail.com>
2026-08-28 20:42:52 -0700

    hooked up totem states, left it cycling by default. comment out coroutine to stop.

---
+++ Assets/Art/interior/totem3/totem_anim_handler.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using System.Collections;
+using System;
+
+public class totem_anim_handler : MonoBehaviour
+{
+    public static totem_anim_handler Instance;
+    private Animator animController;
+    [SerializeField] private Animator sideViewController;
+
+    private void Awake()
+    {
+        if (!Instance)
+        {
+            Instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+    void Start()
+    {
+        animController = GetComponent<Animator>();
+        StartCoroutine(showStates());
+    }
+
+    IEnumerator showStates()
+    {
+        updateState(4);
+        yield return new WaitForSeconds(5);
+        updateState(0);
+        yield return new WaitForSeconds(5);
+        StartCoroutine(showStates());
+    }
+
+    public void updateState(int state)
+    {
+        animController.SetInteger("state", state);
+        sideViewController.SetInteger("state", state);
+    }
+
+}
